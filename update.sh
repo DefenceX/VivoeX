5f7094790f9a5ad1959ad7bad196132a9db617da
Ross Newman <ross.newman@defencex.com.au>
2022-11-12 15:56:09 +1000

    Added cyclone libraries to binary package

--- update.sh
+++ update.sh
@@ -9,8 +9,10 @@ patch=$((${lines[2]}+1))
 after="${lines[0]}\\.${lines[1]}\\.${patch}"
 echo "Updated to ${lines[0]}.${lines[1]}.${lines[2]}"
 sed -i "s/${before}/${after}/g" ../README.md
-sed -i "s/#define PATCH ${lines[2]}/#define PATCH ${patch}/g" ../hmi-display/gva.h
+sed -i "s/#define PATCH ${lines[2]}/#define PATCH ${patch}/g" ../hmi-display/src/gva.h
 echo "${lines[0]}" > ../version
 echo "${lines[1]}" >> ../version
 echo "${patch}" >> ../version
 echo "${tweak}" >> ../version
+# Get the badge
+wget https://img.shields.io/badge/version-${after}-red.svg -O ./build/version.svg
