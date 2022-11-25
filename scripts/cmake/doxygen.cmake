e0ad578f785bea5d53ec9ad82ca045fc6d6f3018
Ross Newman <ross.newman@defencex.com.au>
2022-11-25 21:26:34 +1000

    Adding generated widgets to doxygen

--- scripts/cmake/doxygen.cmake
+++ scripts/cmake/doxygen.cmake
@@ -1,5 +1,13 @@
 
+add_custom_target(doxygen-images COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/test-hmi)
+add_dependencies(doxygen-images test-hmi)
+
+add_custom_target(doxygen-images-convert COMMAND convert ${CMAKE_BINARY_DIR}/*.png -trim +repage  -set filename:f '%t' ${CMAKE_SOURCE_DIR}/docs/'%[filename:f].png')
+add_dependencies(doxygen-images-convert doxygen-images)
+
 set(ENV{'SEM_VERSION'} ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION})
 set(SEM_VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION})
 message(STATUS "Setup doxygen version ${SEM_VERSION} ${GIT_HASH}")
 add_custom_target(doxygen COMMAND SEM_VERSION=${SEM_VERSION} GIT_HASH=${GIT_HASH} doxygen WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+
+add_dependencies(doxygen doxygen-images-convert)
