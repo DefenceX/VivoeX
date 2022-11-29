735485c9905a37f9bcb1ff90881e0ade9852c054
Ross Newman <ross.newman@defencex.com.au>
2022-11-29 18:54:20 +1000

    updated doxygen mainpage

--- scripts/cmake/doxygen.cmake
+++ scripts/cmake/doxygen.cmake
@@ -1,7 +1,7 @@
 
 # The test program is used to ganerate all the widgets, argument 1 is 1millisecond delay in displaying images
 add_custom_target(doxygen-images COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/test-hmi 1)
-add_dependencies(doxygen-images test-hmi)
+add_dependencies(doxygen-images widget-tester)
 
 add_custom_target(doxygen-images-convert COMMAND convert ${CMAKE_BINARY_DIR}/*.png -trim +repage  -set filename:f '%t' ${CMAKE_SOURCE_DIR}/images/doxygen/'%[filename:f].png')
 add_dependencies(doxygen-images-convert doxygen-images)
