1c97c112e81659a82a43fb84f195099e92191b07
Ross Newman <ross.newman@defencex.com.au>
2022-11-30 11:42:21 +1000

    Added logic into cmake to check for changes PNG doxygen images

--- scripts/cmake/doxygen.cmake
+++ scripts/cmake/doxygen.cmake
@@ -12,4 +12,17 @@ set(SEM_VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERS
 message(STATUS "Setup doxygen version ${SEM_VERSION} ${GIT_HASH}")
 add_custom_target(doxygen COMMAND IMAGE_PATH=${CMAKE_BINARY_DIR}/images/doxygen SEM_VERSION=${SEM_VERSION} GIT_HASH=${GIT_HASH} doxygen WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
+# Move any changed images over into source to be checked in
+# We cannot check PNG files directly as the header changes after each generation so do a pixel by pixel check
+file(GLOB PNG_FILES ${CMAKE_BINARY_DIR}/images/doxygen/*.png)
+foreach(FILENAME_FULL ${PNG_FILES})
+  get_filename_component(FILENAME_ONLY ${FILENAME_FULL} NAME)
+  execute_process(COMMAND compare -metric AE ${FILENAME_FULL} ${CMAKE_SOURCE_DIR}/images/doxygen/${FILENAME_ONLY} diff.png ERROR_VARIABLE PIXELS_DIFF OUTPUT_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if (NOT PIXELS_DIFF MATCHES "0")
+    message(STATUS "PNG file '${FILENAME_ONLY}' has changed with ${PIXELS_DIFF} pixels differing, copied into source")
+    execute_process(COMMAND cp ${FILENAME_FULL} ${CMAKE_SOURCE_DIR}/images/doxygen/${FILENAME_ONLY})
+  endif()
+
+endforeach()
+
 add_dependencies(doxygen doxygen-images-convert)
