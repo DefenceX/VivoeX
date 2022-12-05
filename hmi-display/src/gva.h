ab10e15acfec32ccc815e139e5415dcc8717884d
Ross Newman <ross.newman@defencex.com.au>
2022-12-05 19:12:35 +1000

    Updateing doxygen images

--- hmi-display/src/gva.h
+++ hmi-display/src/gva.h
@@ -34,7 +34,7 @@ namespace gva {
 
 static const uint32_t kSemVerMajor = 0;
 static const uint32_t kSemVerMinor = 3;
-static const uint32_t kSemVerPatch = 1295;
+static const uint32_t kSemVerPatch = 1297;
 static const uint32_t kMinimumHeight = 480;
 static const uint32_t kMinimumWidth = 640;
 
