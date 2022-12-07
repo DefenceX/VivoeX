733a9f31d12ad4e9015dd7fdc3489419cf141378
Ross Newman <ross.newman@defencex.com.au>
2022-12-07 16:11:23 +1000

    Adding code coverage

--- test/test_log.cc
+++ test/test_log.cc
@@ -22,7 +22,6 @@
 /// \file test_log.cc
 ///
 
-#define CATCH_CONFIG_MAIN
 #include <unistd.h>
 
 #include <catch/catch.hpp>
@@ -30,22 +29,68 @@
 #include <string>
 
 #include "events_gva.h"
+#include "gtest/gtest.h"
 #include "gva.h"
 #include "log_gva.h"
 
-using namespace gva;
+namespace gva {
 
 static EventsGva *events;
 
-TEST_CASE("Log", "Logging tests") {
+// The fixture for testing class Foo.
+class LogTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if their bodies would
+  // be empty.
+
+  LogTest() {
+    // You can do set-up work for each test here.
+  }
+
+  ~LogTest() override {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  void SetUp() override {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  void TearDown() override {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  // Class members declared here can be used by all tests in the test suite
+  // for Foo.
+};
+
+TEST_F(LogTest, LoggingTests) {
   std::string test1 = "This is a string test";
+  logGva::log(test1, LOG_INFO);
+  logGva::log(test1, LOG_DEBUG);
+  logGva::log(test1, LOG_ERROR);
+  logGva::log(test1, LOG_WARNING);
+  logGva::log(test1, LOG_ERROR);
   char test2[] = "This is a char* test";
-  //  logGva::log(test1, LOG_INFO);
   logGva::log(test2, LOG_INFO);
   logGva::log(test2, LOG_DEBUG);
   logGva::log(test2, LOG_ERROR);
   logGva::log(test2, LOG_WARNING);
+  logGva::log(test2, LOG_ERROR);
   logGva::finish();
+}
+// Tests that Foo does Xyz.
+TEST_F(LogTest, DoesXyz) {
+  // Exercises the Xyz feature of Foo.
+}
 
-  REQUIRE(0 == 0);
+}  // namespace gva
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
