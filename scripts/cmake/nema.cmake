a4a041a2ba66512b829107bc88e293b5a5bd4e95
Ross Newman <ross.newman@defencex.com.au>
2022-12-08 17:07:59 +1000

    Fixing link issues and seperation out cmake files

---
+++ scripts/cmake/nema.cmake
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------------------------
+# libnema
+# ------------------------------------------------------------------------------
+ExternalProject_Add(
+    libnmea
+    GIT_REPOSITORY      https://github.com/DefenceX/nmealib
+    GIT_SHALLOW         1
+    CMAKE_ARGS          ""
+    GIT_TAG             main
+    PREFIX              ${CMAKE_BINARY_DIR}/external/libnmea/prefix
+    TMP_DIR             ${CMAKE_BINARY_DIR}/external/libnmea/tmp
+    STAMP_DIR           ${CMAKE_BINARY_DIR}/external/libnmea/stamp
+    DOWNLOAD_DIR        ${CMAKE_BINARY_DIR}/external/libnmea/download
+    SOURCE_DIR          ${CMAKE_BINARY_DIR}/external/libnmea/src
+    BINARY_DIR          ${CMAKE_BINARY_DIR}/external/libnmea/build
+    INSTALL_COMMAND     ""
+    UPDATE_DISCONNECTED 1
+    BUILD_ALWAYS        0
+)
