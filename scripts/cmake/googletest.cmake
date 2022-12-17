556b91c15e93c28524073a15ebb6d71a9c44b487
Ross Newman <ross.newman@defencex.com.au>
2022-12-17 12:54:40 +1000

    Minor fix for CentOS lib path (differs for gtest on CentOS)

--- scripts/cmake/googletest.cmake
+++ scripts/cmake/googletest.cmake
@@ -22,4 +22,5 @@ ExternalProject_Add(
 
 set(GTEST_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/external/install/usr/local/include)
 set(GTEST_LIBARAIES gmock_main gmock gtest)
-set(GTEST_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/external/install/usr/local/lib)
+# Ubuntu=/usr/local/lib CentOS=/usr/local/lib64, we just set paths for both
+set(GTEST_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/external/install/usr/local/lib ${CMAKE_BINARY_DIR}/external/install/usr/local/lib64)
