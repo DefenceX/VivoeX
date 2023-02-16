60a10612da0a20b7b0468c30e93004a442012302
Ross Newman <ross.newman@defencex.com.au>
2023-02-16 12:10:32 +1000

    Adding css skin to doxygen

--- scripts/init-msys2.sh
+++ scripts/init-msys2.sh
@@ -1,1 +1,6 @@
-pacman -S base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-gtk3  mingw-w64-x86_64-gcc mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-nsis
+pacman -S base-devel \
+  mingw-w64-x86_64-toolchain \
+  mingw-w64-x86_64-gtk3 \
+  mingw-w64-x86_64-gcc mingw-w64-x86_64-ffmpeg \
+  mingw-w64-x86_64-nsis \
+  mingw-w64-x86_64-graphviz
