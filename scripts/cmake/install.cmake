77073b157a47de29fed0418255e1413795be8f03
Ross Newman <ross.newman@defencex.com.au>
2023-02-22 11:01:19 +1000

    Fixing issue with install path for images

--- scripts/cmake/install.cmake
+++ scripts/cmake/install.cmake
@@ -1,6 +1,7 @@
 ## Here you can set the default 'dummy' camera feed images
 set(UK_IMAGE_PATH ${CMAKE_SOURCE_DIR}/images/uk)
 set(AU_IMAGE_PATH ${CMAKE_SOURCE_DIR}/images/au)
+set(IMAGE_PATH ${AU_IMAGE_PATH})
 
 if (MSYS)
   include(${CMAKE_SOURCE_DIR}/scripts/cmake/install-msys.cmake)
