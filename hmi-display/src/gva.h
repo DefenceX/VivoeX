147b0f7d78c56827a446af6c97efb206c9dffd63
Ross Newman <ross.newman@defencex.com.au>
2023-02-23 14:23:25 +1000

    Adding debug to yaml

--- hmi-display/src/gva.h
+++ hmi-display/src/gva.h
@@ -33,7 +33,7 @@ namespace gva {
 
 static const uint32_t kSemVerMajor = 0;
 static const uint32_t kSemVerMinor = 3;
-static const uint32_t kSemVerPatch = 1918;
+static const uint32_t kSemVerPatch = 1919;
 static const uint32_t kMinimumHeight = 480;
 static const uint32_t kMinimumWidth = 640;
 
