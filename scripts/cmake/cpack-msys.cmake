4c8be5d57ff61e52e74963bc6f5699135084167a
Ross Newman <ross.newman@defencex.com.au>
2023-02-27 20:44:32 +1000

    Fixing MSYS Cpack

--- scripts/cmake/cpack-msys.cmake
+++ scripts/cmake/cpack-msys.cmake
@@ -1,4 +1,4 @@
-PROJECT(vivoe-lite)
+project(${CPACK_PACKAGE_NAME})
 
 set(CPACK_GENERATOR "NSIS")
 
@@ -6,7 +6,7 @@ set(CPACK_GENERATOR "NSIS")
 set(CPACK_SYSTEM_NAME win64)
 # set(CPACK_SET_DESTDIR FALSE)
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "vivoe-lite")
-set(CPACK_COMPONENTS_ALL COMPONENTS)
+#set(CPACK_COMPONENTS_ALL COMPONENTS)
 set(CPACK_COMPONENT_APPLICATIONS_DISPLAY_NAME "Vivoe Lite")
 # set(CPACK_PACKAGING_INSTALL_PREFIX "vivoe-lite")
 
@@ -21,7 +21,7 @@ set(CPACK_NSIS_CONTACT enquiries@defencex.ai)
 set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
 set(CPACK_NSIS_MODIFY_PATH OFF)
 set(CPACK_NSIS_DISPLAY_NAME "Vivoe Lite installer")
-set(CPACK_NSIS_PACKAGE_NAME "Vivoe Lite installer")
+set(CPACK_NSIS_PACKAGE_NAME ${CPACK_PACKAGE_NAME})
 set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/LICENSE)
 set(CPACK_NSIS_INSTALLED_ICON_NAME ${CMAKE_SOURCE_DIR}/images/favicon.ico)
 set(CPACK_NSIS_HELP_LINK "https://defencex.ai/downloads.html")
@@ -30,43 +30,4 @@ set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
     CreateShortCut \\\"$DESKTOP\\\\${PROJECT_NAME}.lnk\\\" \\\"$INSTDIR\\\\bin\\\\hmi-display.exe\\\"
 ")
 set(CPACK_NSIS_CONTACT "enquiries@defencex.com.au")
-
-
-# set(CPACK_NSIS_MENU_LINKS
-#     "doc/cmake-@CMake_VERSION_MAJOR@.@CMake_VERSION_MINOR@/cmake.html"
-#     "CMake Help" "https://cmake.org" "CMake Web Site")
-set(CPACK_NSIS_WELCOME_TITLE "Vivoe Lite (DefenceX)")
-# set(CPACK_NSIS_WELCOME_TITLE_3LINES  "three\nwelcome\nlines")
-set(CPACK_NSIS_FINISH_TITLE "Installation complete...")
-# set(CPACK_NSIS_FINISH_TITLE_3LINES "three\nfinish\nlines")
-# set(CPACK_NSIS_MUI_HEADERIMAGE "")
-# set(CPACK_NSIS_BRANDING_TEXT "This is some branding text")
-
-# WIX
-set(CPACK_WIX_PRODUCT_GUID "a19cc271-5031-4579-b0a1-d0f541354291") 
-set(CPACK_WIX_PRODUCT_ICON ${CMAKE_SOURCE_DIR}/images/favicon.ico)
-set(CPACK_WIX_LICENSE_RTF ${CMAKE_SOURCE_DIR}/LICENSE)
-
-# set(CPACK_COMPONENT_LIBRARIES_DISPLAY_NAME "Libraries")
-# set(CPACK_COMPONENT_HEADERS_DISPLAY_NAME "C++ Headers")
-# # Human readable component descriptions
-# set(CPACK_COMPONENT_APPLICATIONS_DESCRIPTION
-#   "An extremely useful application that makes use of MyLib")
-# set(CPACK_COMPONENT_LIBRARIES_DESCRIPTION
-#   "Static libraries used to build programs with MyLib")
-# set(CPACK_COMPONENT_HEADERS_DESCRIPTION
-#   "C/C++ header files for use with MyLib")
-# # Define dependencies between components
-# set(CPACK_COMPONENT_HEADERS_DEPENDS libraries)
-# # Define groups
-# set(CPACK_COMPONENT_APPLICATIONS_GROUP "Runtime")
-# set(CPACK_COMPONENT_LIBRARIES_GROUP "Development")
-# set(CPACK_COMPONENT_HEADERS_GROUP "Development")
-# set(CPACK_COMPONENT_GROUP_DEVELOPMENT_DESCRIPTION
-#    "All of the tools you'll ever need to develop software")
-# # Define NSIS installation types
-# set(CPACK_ALL_INSTALL_TYPES Full Developer)
-# set(CPACK_COMPONENT_LIBRARIES_INSTALL_TYPES Developer Full)
-# set(CPACK_COMPONENT_HEADERS_INSTALL_TYPES Developer Full)
-# set(CPACK_COMPONENT_APPLICATIONS_INSTALL_TYPES Full)
 
